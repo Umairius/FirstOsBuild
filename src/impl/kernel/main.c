ca42eb0d092c17681211fe4e7880b2e899ea84d3
Umairius <mumair.bscs19seecs@seecs.edu.pk>
2021-04-14 17:45:35 +0500

    Os Complete build
    Hehe boi

---
+++ src/impl/kernel/main.c
@@ -0,0 +1,15 @@
+void kernel_main(){
+
+    print_clear(); 
+    print_set_color('PRINT_COLOR_YELLOW' , 'PRINT_COLOR_BLACK');
+    print_str("WELCOME TO OUR 64-BIT KERNEL BAMCHA\n\n\n\n");
+
+    print_str("*      *  *       * *********  ************\n");
+    print_str("* *    *  *       * |                *     \n");
+    print_str("*   *  *  *       * *********        *     \n");
+    print_str("*    * *  *       *         |        *     \n");
+    print_str("*      *  ********* *********        *     \n");
+
+
+    print_str("Muhammad Umair CMS_ID 282593 BSCS-9B");
+}
